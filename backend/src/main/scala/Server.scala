b3472d618a9b58fcec2a0b49d2b115e462f0efb0
Liyeuk Reynald <reynaldjoabet@gmail.com>
2022-04-29 09:57:03 +0400

    more setup

--- backend/src/main/scala/Server.scala
+++ backend/src/main/scala/Server.scala
@@ -1,5 +1,4 @@
 import cats.effect.{ExitCode, IO, IOApp}
-
 object Server extends  IOApp{
   override def run(args: List[String]): IO[ExitCode] =
     IO.delay(println("hello dude")).as(ExitCode.Success)
